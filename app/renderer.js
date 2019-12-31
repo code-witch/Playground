b90030fc6bde9e4be7b96b70e351bc89daf6fa84
code-witch <code-witch@users.noreply.github.com>
2019-12-30 20:25:02 -0700

    Add canvas

--- app/renderer.js
+++ app/renderer.js
@@ -1,4 +1,5 @@
 const loader = require('monaco-loader');
+const canvas = document.getElementById('main-canvas')
 let monacoEditor;
 
 loader().then((monaco) => {
@@ -10,6 +11,11 @@ loader().then((monaco) => {
         value: "let help = 'me';"
     }); 
 }).then(() => {  
-    // need a way to access `monacoEditor` from outside this file..
+    // need a way to access `monacoEditor` from outside this file.. (maybe)
     // exports.getValue = () => monacoEditor.getValue();
+
+    // this might be wrong / weird
+    let ctx = canvas.getContext('2d')
+    ctx.fillStyle = 'dodgerblue';
+    ctx.fillRect(50,50,50,50);
 });
